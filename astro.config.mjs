d6e01bf96643dac3b11355daead82a5872f74213
Samanyu A Saji <samanyusaji18@gmail.com>
2026-01-18 19:56:26 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  site: 'https://samanyu13.github.io',
+  base: '/',
 });
